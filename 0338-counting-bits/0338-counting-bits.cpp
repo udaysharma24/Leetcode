336231bb3a8d6bec0292ab4f141a8d27d1d88320
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-06-15 12:52:16 +0530

    Time: 3 ms (22.46%), Space: 18 MB (7.48%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dp=vector<int>(100001,-1);
+    int bits(int num){
+        if(dp[num]!=-1)
+            return dp[num];
+        if(num==0)
+            return dp[num]=0;
+        if(num==1)
+            return dp[num]=1;
+        int temp=num;
+        if(temp%2==1)
+            return dp[num]=1+bits(temp/2);
+        return dp[num]=bits(temp/2);
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0; i<=n; i++){
+            if(dp[i]!=-1)
+                ans.push_back(dp[i]);
+            ans.push_back(bits(i));
+        }
+        return ans;
+    }
+};

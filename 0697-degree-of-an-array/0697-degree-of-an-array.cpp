bb4aa9257190117a5850ef860b27800978e87764
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-07-20 17:36:18 +0530

    Time: 19 ms (22.2%), Space: 31.8 MB (11.1%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        unordered_map<int,int> um;
+        int mx=0;
+        set<int> maxnums;
+        for(int i=0; i<nums.size(); i++){
+            um[nums[i]]++;
+            mx=max(mx,um[nums[i]]);
+        }
+        map<int,int> firstindex;
+        map<int,int> lastindex;
+        for(int i=0; i<nums.size(); i++){
+            if(um[nums[i]]==mx)
+                maxnums.insert(nums[i]);
+            if(firstindex.find(nums[i])==firstindex.end())
+                firstindex[nums[i]]=i;
+        }
+        for(int i=nums.size()-1; i>=0; i--){
+            if(lastindex.find(nums[i])==lastindex.end())
+                lastindex[nums[i]]=i;
+        }
+        int subarraysize=1e5;
+        auto it=maxnums.begin();
+        while(it!=maxnums.end()){
+            subarraysize=min(lastindex[*it]-firstindex[*it]+1,subarraysize);
+            it++;
+        }
+        return subarraysize;
+    }
+};

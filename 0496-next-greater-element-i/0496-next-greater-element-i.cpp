9acc27228d30048d6c97a0238700785756fe7034
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-08-02 07:39:14 +0530

    Time: 3 ms (40.7%), Space: 12.8 MB (40.07%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int m=nums1.size();
         int n=nums2.size();
-        vector<int> nge(n);
+        unordered_map<int,int> um;
         stack<int> ms;
         for(int i=n-1; i>=0; i--){
             if(ms.empty()){
                 ms.push(i);
-                nge[i]=-1;
+                um[nums2[i]]=-1;
             }
             else if(nums2[i]>nums2[ms.top()]){
                 while(!ms.empty() && nums2[i]>nums2[ms.top()]){
@@ -16,29 +16,21 @@ public:
                 }
                 if(ms.empty()){
                     ms.push(i);
-                    nge[i]=-1;
+                    um[nums2[i]]=-1;
                 }
                 else{
-                    nge[i]=nums2[ms.top()];
+                    um[nums2[i]]=nums2[ms.top()];
                     ms.push(i);
                 }
             }
             else if(nums2[i]<nums2[ms.top()]){
-                nge[i]=nums2[ms.top()];
+                um[nums2[i]]=nums2[ms.top()];
                 ms.push(i);
             }
         }
-        for(int j=0; j<n; j++){
-            cout<<nge[j]<<" ";
-        }
         vector<int> ans(m);
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(nums1[i]==nums2[j]){
-                    ans[i]=nge[j];
-                    break;
-                }
-            }
+            ans[i]=um[nums1[i]];
         }
         return ans;
     }

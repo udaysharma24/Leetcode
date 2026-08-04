dbaf47380a5949501d4e03a7b7c85529cd530016
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-08-04 06:51:06 +0530

    Time: 11 ms (6.06%), Space: 34.5 MB (5.13%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        vector<int> v;
+        sort(nums.begin(),nums.end());
+        vector<int> missing;
         unordered_map<int,int> um;
         for(int i=0; i<nums.size(); i++){
             um[nums[i]]++;
         }
-        int mn=*min_element(nums.begin(),nums.end());
+        int small=nums[0]+1;
         int mx=*max_element(nums.begin(),nums.end());
-        for(int i=mn; i<=mx; i++){
-            if(um[i]==0)
-                v.push_back(i);
+        while(small<mx){
+            if(um[small]==0)
+                missing.push_back(small);
+            small++;
         }
-        return v;
+        return missing;
     }
 };

339b9d0aa4f7f113e6a25d94ec13d5359431faf8
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-08-06 12:23:04 +0530

    Time: 0 ms (100%), Space: 10.6 MB (13.31%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
@@ -1,22 +1,49 @@
 class Solution {
 public:
     int secondsToRemoveOccurrences(string s) {
-        int seconds=0;
-        int left=0;
-        bool remaining=true;;
-        while(remaining){
-            remaining=false;
-            for(int j=0; j<s.length()-1; j++){
-                if(s[j]=='0' && s[j+1]=='1'){
-                    s[j]='1';
-                    s[j+1]='0';
-                    j++;
-                    remaining=true;
-                }
-            }
-            if(remaining==true)
-                seconds++;
+        // int seconds=0;
+        // int left=0;
+        // bool remaining=true;;
+        // while(remaining){
+        //     remaining=false;
+        //     for(int j=0; j<s.length()-1; j++){
+        //         if(s[j]=='0' && s[j+1]=='1'){
+        //             s[j]='1';
+        //             s[j+1]='0';
+        //             j++;
+        //             remaining=true;
+        //         }
+        //     }
+        //     if(remaining==true)
+        //         seconds++;
+        // }
+        // return seconds;
+        int n=s.length();
+        vector<vector<int>> dp(2,vector<int>(n,0));
+        bool zero=false;
+        if(s[0]=='0'){
+            dp[0][0]=1;
+            dp[1][0]=0;
+            zero=true;
         }
-        return seconds;
+        else if(s[0]=='1'){
+            dp[0][0]=0;
+            dp[1][0]=0;
+        }
+        for(int i=1; i<n; i++){
+            if(s[i]=='0'){
+                dp[0][i]=dp[0][i-1]+1;
+                dp[1][i]=dp[1][i-1];
+                zero=true;
+            }
+            else{
+                dp[0][i]=dp[0][i-1];
+                if(zero)
+                    dp[1][i]=max(dp[0][i-1],dp[1][i-1]+1);
+                else
+                    dp[1][i]=dp[1][i-1];
+            }
+        }
+        return dp[1][n-1];
     }
 };

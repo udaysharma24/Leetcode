89762a3be78fe4faf3aef641afd88b35a6975bb3
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-08-07 07:11:19 +0530

    Time: 0 ms (100%), Space: 10.1 MB (88.47%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -3,87 +3,43 @@ public:
     stack<int> s1;
     stack<int> s2;
     MyQueue() {
-        
+        stack<int> s1;
+        stack<int> s2;
     }
     
     void push(int x) {
-        if(s1.empty())
-            s2.push(x);
-        else
-            s1.push(x);
-        return;
+        s1.push(x);
     }
     
     int pop() {
-        if(s1.empty()==false)
-        {
-            while(s1.size()>1)
-            {
-                s2.push((s1.top()));
-                s1.pop();
-            }
-            int temp= s1.top();
+        while(!s1.empty()){
+            s2.push(s1.top());
             s1.pop();
-            while(s2.empty()==false)
-            {
-                s1.push((s2.top()));
-                s2.pop();
-            }
-            return temp;
         }
-        else
-        {
-            while(s2.size()>1)
-            {
-                s1.push((s2.top()));
-                s2.pop();
-            }
-            int temp= s2.top();
+        int temp=s2.top();
+        s2.pop();
+        while(!s2.empty()){
+            s1.push(s2.top());
             s2.pop();
-            while(s1.empty()==false)
-            {
-                s2.push((s1.top()));
-                s1.pop();
-            }
-            return temp;
         }
+        return temp;
     }
     
     int peek() {
-        if(s1.empty()==false)
-        {
-            while(s1.size()>1)
-            {
-                s2.push((s1.top()));
-                s1.pop();
-            }
-            int temp= s1.top();
-            while(s2.empty()==false)
-            {
-                s1.push((s2.top()));
-                s2.pop();
-            }
-            return temp;
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
         }
-        else
-        {
-            while(s2.size()>1)
-            {
-                s1.push((s2.top()));
-                s2.pop();
-            }
-            int temp= s2.top();
-            while(s1.empty()==false)
-            {
-                s2.push((s1.top()));
-                s1.pop();
-            }
-            return temp;
+        int temp=s2.top();
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
         }
+        return temp;
     }
     
     bool empty() {
-        if(s1.size()==0 && s2.size()==0)
+        if(s1.empty())
             return true;
         else
             return false;

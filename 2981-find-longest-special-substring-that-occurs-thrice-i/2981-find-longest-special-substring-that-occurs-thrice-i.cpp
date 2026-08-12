954ab31e0e135167de6a6296c65a22d9b7114e2c
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-08-12 13:53:52 +0530

    Time: 103 ms (8.74%), Space: 12.2 MB (58.13%) - LeetHub

---
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maximumLength(string s) {
+        map<string,int> um;
+        string str="";
+        str.push_back(s[0]);
+        um[str]++;
+        for(int j=1; j<s.length(); j++){
+            if(s[j]==s[j-1]){
+                str.push_back(s[j]);
+                um[str]++;
+            }
+            else
+                break;
+        }
+        for(int i=1; i<s.length(); i++){
+            str="";
+            str.push_back(s[i]);
+            um[str]++;
+            for(int j=i+1; j<s.length(); j++){
+                if(s[j]==s[i]){
+                    str.push_back(s[j]);
+                    um[str]++;
+                }
+                else
+                    break;
+            }
+        }
+        int maxsize=-1;
+        for(const auto& [key,value]:um){
+            cout<<key<<":"<<value<<"\n";
+            if(value>=3)
+                maxsize=max(maxsize,int(key.length()));
+        }
+        return maxsize;
+    }
+};

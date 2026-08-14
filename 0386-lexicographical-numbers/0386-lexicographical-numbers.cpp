c320c89d27f257084db98f1e5b288e57b0ac9a95
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-08-14 13:08:32 +0530

    Time: 0 ms (100%), Space: 14.1 MB (46.68%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
+    void solve(int startnum, int n, vector<int>& v){
+        int cur=startnum;
+        if(cur<=n)
+            v.push_back(cur);
+        else
+            return;
+        for(int i = 0; i <= 9; i++){
+            int next = cur*10+i;
+            if(next>n)
+                break;
+            solve(next,n,v);
+        }
+    }
     vector<int> lexicalOrder(int n) {
-        priority_queue<string, vector<string>, greater<string>> pq;
-        vector<int> ans;
-        for(int i=1; i<=n; i++){
-            pq.push(to_string(i));
+        // priority_queue<string, vector<string>, greater<string>> pq;
+        // vector<int> ans;
+        // for(int i=1; i<=n; i++){
+        //     pq.push(to_string(i));
+        // }
+        // while(!pq.empty()){
+        //     ans.push_back(stoi(pq.top()));
+        //     pq.pop();
+        // }
+        // return ans;
+
+        vector<int> v;
+        for(int i=1; i<=min(9,n); i++){
+            solve(i,n,v);
         }
-        while(!pq.empty()){
-            ans.push_back(stoi(pq.top()));
-            pq.pop();
-        }
-        return ans;
+        return v;
     }
 };

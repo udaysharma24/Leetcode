7f1ddd0b3cc54236757f1f7d44542f2f59764de3
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-08-19 12:41:30 +0530

    Time: 32 ms (27.97%), Space: 63.9 MB (72.03%) - LeetHub

--- 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.cpp
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.cpp
@@ -2,23 +2,11 @@ class Solution {
 public:
     int maxNumOfMarkedIndices(vector<int>& nums) {
         int n=nums.size();
-        vector<bool> visited(n,false);
         sort(nums.begin(),nums.end());
-        int index=0;
-        for(int i=0; i<n; i++){
-            if(2*nums[i]>nums[n-1]){
-                index=i;
-                break;
-            }
-        }
-        // for(int i=0; i<n; i++){
-        //     cout<<nums[i]<<" ";
-        // }
         int l=0;
-        int r=index;
+        int r=(n+1)/2;
         int marked=0;
-        int maxmarked=0;
-        while(l<index && r<n){
+        while(l<r && r<n){
             if(nums[r]>=2*nums[l]){
                 l++;
                 r++;
@@ -27,21 +15,6 @@ public:
             else
                 r++;
         }
-        maxmarked=max(marked,maxmarked);
-        l=0;
-        r=(n+1)/2;
-        marked=0;
-        maxmarked=0;
-        while(l<index && r<n){
-            if(nums[r]>=2*nums[l]){
-                l++;
-                r++;
-                marked+=2;
-            }
-            else
-                r++;
-        }
-        maxmarked=max(marked,maxmarked);
-        return maxmarked;
+        return marked;
     }
 };

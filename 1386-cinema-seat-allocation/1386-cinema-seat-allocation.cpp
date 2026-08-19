9bcfdf5f93426dd658c18daa642cef72425d05e8
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-08-19 11:11:51 +0530

    Time: 183 ms (5.07%), Space: 79.1 MB (28.04%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        sort(reservedSeats.begin(),reservedSeats.end(),[](vector<int>& a, vector<int>& b){
+            if(a[0]==b[0])
+                return a[1]<b[1];
+            return a[0]<b[0];
+        });
+        unordered_map<int,bool> um;
+        unordered_map<int,bool> uml;
+        unordered_map<int,bool> umu;
+        unordered_map<int,bool> umm;
+        for(int i=0; i<reservedSeats.size(); i++){
+            int row=reservedSeats[i][0];
+            int col=reservedSeats[i][1];
+            um[row]=true;
+            if(col!=1 && col<=5)
+                uml[row]=true;
+            if(col!=10 && col>=6)
+                umu[row]=true;
+            if(col>=4 && col<=7)
+                umm[row]=true;
+        }
+        int ans=2*n;
+        for(const auto &[row,reserved]:um){
+            if(uml[row] && umu[row] && umm[row]){
+                ans-=2;
+                cout<<"part1\n";
+            }
+            else if(uml[row] && umu[row]){
+                ans--;
+                cout<<"part2\n";
+            }
+            else if(uml[row] && umm[row]){
+                ans--;
+                cout<<"part3\n";
+            }
+            else if(umm[row] && umu[row]){
+                ans--;
+                cout<<"part4\n";
+            }
+            else if(uml[row] || umm[row] || umu[row]){
+                ans--;
+                cout<<"part5\n";
+            }
+        }
+        return ans;
+    }
+};

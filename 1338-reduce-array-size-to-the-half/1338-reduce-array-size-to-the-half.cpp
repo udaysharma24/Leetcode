e162e6369ad12863bf442869afc3b666d6d91c94
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-09-01 13:51:06 +0530

    Time: 352 ms (5.07%), Space: 89.3 MB (5.62%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int> um;
+        for(int i=0; i<arr.size(); i++){
+            um[arr[i]]++;
+        }
+        sort(arr.begin(),arr.end(),[&um](int a, int b){
+            if(um[a]==um[b])
+                return a<b;
+            return um[a]>um[b];
+        });
+        // for(int i=0; i<arr.size(); i++){
+        //     cout<<arr[i]<<" ";
+        // }
+        // cout<<"\n";
+        unordered_set<int> us;
+        int ans=0;
+        int count=0;
+        for(int i=0; i<arr.size(); i++){
+            if(us.find(arr[i])==us.end()){
+                us.insert(arr[i]);
+                count+=um[arr[i]];
+                // cout<<arr[i]<<" ";
+                ans++;
+            }
+            if(2*count>=arr.size())
+                break;
+        }
+        return ans;
+    }
+};

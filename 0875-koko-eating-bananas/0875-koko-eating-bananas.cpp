cae874fdd351e839ab8370a9ad306d660e6f6a48
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-09-09 14:15:50 +0530

    Time: 22 ms (5.53%), Space: 22.9 MB (90.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    long long int hourstaken(vector<int> piles, int mid){
-        long long int hours=0;
-        for(int i=0; i<piles.size(); i++){
-            hours+=ceil((double)piles[i]/mid);
-        }
-        return hours;
-    }
     int minEatingSpeed(vector<int>& piles, int h) {
-        sort(piles.begin(),piles.end());
         int n=piles.size();
+        sort(piles.begin(),piles.end());
         int low=1;
         int high=piles[n-1];
+        int finalans=INT_MAX;
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(hourstaken(piles,mid)<=(long long)h)
+            int mid=(low+high)/2;
+            long long int ans=0;
+            for(int i=0; i<n; i++){
+                if(piles[i]%mid==0)
+                    ans+=(piles[i]/mid);
+                else
+                    ans+=(piles[i]/mid)+1;
+            }
+            cout<<ans<<"\t"<<mid<<"\n";
+            if(ans<=h){
+                finalans=min(mid,finalans);
                 high=mid-1;
+            }
             else
                 low=mid+1;
         }
-        return low;
+        return finalans;
     }
 };

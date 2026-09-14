18929853e1b52d8969c5ac30b3b995bcc4c6af6e
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-09-14 18:19:47 +0530

    Time: 31 ms (95.31%), Space: 46.8 MB (31.03%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,44 +2,21 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
-        // if(n==1){
-        //     if(nums[0]!=k)
-        //         return 0;
-        //     else
-        //         return 1;
-        // }
-        // vector<int> psum(n);
-        // psum[0]=nums[0];
-        // int cnt=0;
-        // for(int i=1; i<nums.size(); i++){
-        //     psum[i]=psum[i-1]+nums[i];
-        //     if(psum[i]==k)
-        //         cnt++;
-        // }
-        // int l=0;
-        // int r=0;
-        // while(l<=r && r<n){
-        //     if(psum[r]-psum[l]<k)
-        //         r++;
-        //     else if(psum[r]-psum[l]==k){
-        //         cnt++;
-        //         r++;
-        //     }
-        //     else
-        //         l++;
-        // }
-        // return cnt;
-
+        vector<int> psum(n,0);
         unordered_map<int,int> um;
-        um[0]=1;
-        int psum=0;
-        int cnt=0;
-        for(int i=0; i<nums.size(); i++){
-            psum+=nums[i];
-            if(um.find(psum-k)!=um.end())
-                cnt+=um[psum-k];
-            um[psum]++;
+        int ans=0;
+        psum[0]=nums[0];
+        um[psum[0]]=1;
+        if(psum[0]==k)
+            ans++;
+        for(int i=1; i<n; i++){
+            psum[i]=psum[i-1]+nums[i];
+            if(psum[i]==k)
+                ans++;
+            if(um.find(psum[i]-k)!=um.end())
+                ans+=um[psum[i]-k];
+            um[psum[i]]++;
         }
-        return cnt;
+        return ans;
     }
 };

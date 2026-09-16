7318d179e41b1e4bf3a9d75000730f92910184ec
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-09-16 08:11:48 +0530

    Time: 0 ms (100%), Space: 9.7 MB (83.52%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -3,7 +3,7 @@ public:
     string gcdOfStrings(string str1, string str2) {
         if(str1+str2!=str2+str1)
             return "";
-        string gcdstring=str1.substr(0,gcd(str1.length(),str2.length()));
+        string gcdstring=str2.substr(0,gcd(str1.length(),str2.length()));
         return gcdstring;
     }
 };

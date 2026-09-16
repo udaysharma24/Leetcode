64f7dcc17062622a70a3879f44a46f4a92b6dd7e
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-09-16 15:13:45 +0530

    Time: 0 ms (100%), Space: 26.4 MB (8.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size()==1)
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int ans;
+        if(n==1)
             return nums[0];
         else if(nums[0]!=nums[1])
             return nums[0];
-        else if(nums[nums.size()-1]!=nums[nums.size()-2])
-            return nums[nums.size()-1];
-        else
-        {
-            int low=1;
-            int high=nums.size()-2;
+        else if(nums[n-1]!=nums[n-2])
+            return nums[n-1];
+        while(low<=high){
             int mid=(low+high)/2;
-            while(low<=high)
-            {
-                if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
-                    return nums[mid];
-                else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]))
-                {
-                    low=mid+1;
-                    mid=(low+high)/2;
-                }
-                else if((mid%2==0 && nums[mid]==nums[mid-1]) || (mid%2==1 && nums[mid]==nums[mid+1]))
-                {
-                    high=mid-1;
-                    mid=(low+high)/2;
-                }
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                ans=nums[mid];
+                break;
             }
-            return -1;
+            else if(mid%2==0 && nums[mid]==nums[mid-1])
+                high=mid-1;
+            else if(mid%2==0 && nums[mid]==nums[mid+1])
+                low=mid+1;
+            else if(mid%2==1 && nums[mid]==nums[mid-1])
+                low=mid+1;
+            else if(mid%2==1 && nums[mid]==nums[mid+1])
+                high=mid-1;
+            cout<<mid<<"\n";
         }
+        return ans;
     }
 };

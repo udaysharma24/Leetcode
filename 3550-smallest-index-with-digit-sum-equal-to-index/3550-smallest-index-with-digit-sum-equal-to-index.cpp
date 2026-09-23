0401b61174a1ac2f4acc2a52f4c4b7aaac27c413
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-09-23 07:21:13 +0530

    Time: 0 ms (100%), Space: 31.6 MB (3.93%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int smallestIndex(vector<int>& nums) {
-        vector<int> digits(nums.size());
         for(int i=0; i<nums.size(); i++){
             string s=to_string(nums[i]);
             int sum=0;

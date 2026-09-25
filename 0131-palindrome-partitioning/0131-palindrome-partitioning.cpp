3f6832e160472cd659f5b708c6352b3df24bbdc4
udaysharma24 <155619980+udaysharma24@users.noreply.github.com>
2026-09-25 15:25:52 +0530

    Time: 28 ms (92.18%), Space: 53.4 MB (82.93%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-    bool palindrome(string cur){
-        int l=0;
-        int r=cur.length()-1;
-        if(cur.length()==0)
-            return false;
-        bool flag=true;
-        while(l<r){
-            if(cur[l]==cur[r]){
-                l++;
-                r--;
-            }
-            else{
-                flag=false;
-                break;
-            }
+    bool ispalindrome(string s){
+        int n=s.length();
+        for(int i=0; i<n/2; i++){
+            if(s[i]!=s[n-i-1])
+                return false;
         }
-        return flag;
+        return true;
     }
-    void solve(string cur, string s, int index, vector<string>& news, vector<vector<string>>& ans) {
-        if(index == s.length()) {
-            ans.push_back(news);
+    void solve(int index, string& s, vector<string>& vs, vector<vector<string>>& ans){
+        if(index==s.length()){
+            ans.push_back(vs);
             return;
         }
-        for(int end=index; end<s.length(); end++) {
-            cur=s.substr(index, end-index+1);
-            if(palindrome(cur)) {
-                news.push_back(cur);
-                solve(cur,s,end+1,news,ans);
-                news.pop_back();
+        for(int i=index; i<s.length(); i++){
+            string st = s.substr(index, i-index+1);
+            if(ispalindrome(st)){
+                vs.push_back(st);
+                solve(i+1, s, vs, ans);
+                vs.pop_back();   
             }
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> news;
-        int index=0;
-        solve("",s,index,news,ans);
+        vector<string> vs;
+        solve(0, s, vs, ans);
         return ans;
     }
 };
